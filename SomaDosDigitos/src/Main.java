f9ca684bf6145fafa54e93537ad5e930692f9e7a
Savio Ferreira <saviofc.dev@gmail.com>
2024-12-17 19:45:05 -0300

    resolução do desafio utilizando manipulação de strings.

---
+++ SomaDosDigitos/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String somarDigitosPositivos(String value) {
+
+        StringBuilder result = new StringBuilder();
+        int sum = 0;
+
+        if((Integer.parseInt(value)) < 0) {
+           throw new RuntimeException("O número deve ser positivo");
+        } else if (value.length() == 1) {
+            return String.format("%s + 0 = %s", value, value);
+        } else {
+            for (int i = 0; i < value.length(); i++) {
+                result.append(value.charAt(i)).append(" + ");
+                sum += Character.getNumericValue(value.charAt(i));
+            }
+        }
+
+        result.delete(result.length() -3, result.length());
+        return result.append(" = ").append(sum).toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número para a soma de seus valores: ");
+        System.out.println(somarDigitosPositivos(sc.nextLine()));
+        sc.close();
+    }
+
+}
